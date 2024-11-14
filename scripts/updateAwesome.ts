9113aa19703970476e5b5db45a0994bf6beb2121
meDev97 <59795165+meDev97@users.noreply.github.com>
2024-11-14 11:20:39 +0000

    add comment

--- scripts/updateAwesome.ts
+++ scripts/updateAwesome.ts
@@ -1,16 +1,21 @@
-import { readFileSync, writeFileSync } from 'node:fs';
-import { resolve } from 'node:path';
+import { writeFileSync } from "node:fs";
+import { resolve } from "node:path";
 
-import { indexCnPath, indexPath, publicDir, readmeCnPath, readmePath } from './const';
-import { readJSON, updateAwesomeReadme } from './utils';
+import { publicDir } from "./const";
+import { updateAwesomeReadme } from "./utils";
 
-const updateAwesome = (filePath: string, md: string, plugins, locale?: string) => {
+const updateAwesome = (
+  filePath: string,
+  md: string,
+  plugins,
+  locale?: string
+) => {
   const data: string[] = [];
 
   plugins.forEach(({ identifier, author, createdAt, homepage, meta }, i) => {
     const pluginConfigPath = resolve(
       publicDir,
-      [identifier, locale, 'json'].filter(Boolean).join('.'),
+      [identifier, locale, "json"].filter(Boolean).join(".")
     );
     const header = `### ${meta.title}`;
     const subHeader = `<sup>By **[@${author}](${homepage})** on **${createdAt}**</sup>`;
@@ -19,33 +24,33 @@ const updateAwesome = (filePath: string, md: string, plugins, locale?: string) =
       `${meta.tags
         .filter(Boolean)
         .map((tag) => `\`${tag}\``)
-        .join(' ')}`,
-    ].join('\n\n');
+        .join(" ")}`,
+    ].join("\n\n");
 
     const body: string = [
-      i !== 0 ? '---' : false,
+      i !== 0 ? "---" : false,
       header,
       subHeader,
       desc,
       `<div align="right">\n\n[![][back-to-top]](#readme-top)\n\n</div>`,
     ]
       .filter(Boolean)
-      .join('\n\n');
+      .join("\n\n");
     data.push(body);
   });
 
-  const newMd = updateAwesomeReadme(md, data.join('\n\n'));
+  const newMd = updateAwesomeReadme(md, data.join("\n\n"));
 
-  writeFileSync(filePath, newMd, 'utf-8');
+  writeFileSync(filePath, newMd, "utf-8");
 };
 
 const runUpdateAwesome = () => {
-  const readmeCn = readFileSync(readmeCnPath, 'utf-8');
-  const readme = readFileSync(readmePath, 'utf-8');
-  const index = readJSON(indexPath);
-  const indexCn = readJSON(indexCnPath);
-  updateAwesome(readmePath, readme, index.plugins);
-  updateAwesome(readmeCnPath, readmeCn, indexCn.plugins, 'zh-CN');
+  // const readmeCn = readFileSync(readmeCnPath, 'utf-8');
+  // const readme = readFileSync(readmePath, 'utf-8');
+  // const index = readJSON(indexPath);
+  // const indexCn = readJSON(indexCnPath);
+  // updateAwesome(readmePath, readme, index.plugins);
+  // updateAwesome(readmeCnPath, readmeCn, indexCn.plugins, 'zh-CN');
 };
 
 runUpdateAwesome();
